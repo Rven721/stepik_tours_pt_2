fa566ce31ed89294a3de50db5bae9a6ccb34177f
Rven721 <amalyavin1@gmail.com>
2021-03-11 09:53:36 +0300

    Error hendlers

--- tours/views.py
+++ tours/views.py
@@ -2,6 +2,8 @@ from random import randint
 
 from django.shortcuts import render
 
+from django.http import Http404
+
 from .data import departures, tours
 
 
@@ -20,17 +22,19 @@ def main_view(request):
 
 
 def departure_view(request, dep):
+    if dep not in departures:
+        raise Http404
     dep_list = {}
     for key, value in tours.items():
         if value['departure'] == dep:
             dep_list[key] = value
     for value in dep_list.values():
         value['star_img'] = "★" * int(value['stars'])
-    prizes = [int(value['price']) for value in dep_list.values()]
+    prises = [int(value['price']) for value in dep_list.values()]
     nights = [int(value['nights']) for value in dep_list.values()]
     context = {
-        'min_prize': min(prizes),
-        'max_prize': max(prizes),
+        'min_prise': min(prises),
+        'max_prise': max(prises),
         'min_nights': min(nights),
         'max_nights': max(nights),
         'count': len(dep_list),
@@ -41,6 +45,8 @@ def departure_view(request, dep):
 
 
 def tour_view(request, tour_id):
+    if int(tour_id) not in tours:
+        raise Http404
     tour = tours[int(tour_id)]
     stars = "★" * int(tour['stars'])
     run = str(f"{tour['country']} {departures[tour['departure']]} {tour['nights']} ночей")
